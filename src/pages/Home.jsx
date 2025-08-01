149139451c4cc3facdb0f80cbdb8cbf45d654ae6
abhilash2200 <ab.digitalwolf@gmail.com>
2025-08-01 19:19:09 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
-import Loader from '../components/Loader';
+import React from 'react';  
+import Layout from '../components/Layout';
 
 const Home = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  setTimeout(() => {
-    setData({ message: "Hello from API!" });
-    setLoading(false);
-  }, 2000);
-
-  if (loading) return <Loader />;
 
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">{data.message}</h1>
-    </div>
+    <Layout>
+
+    </Layout>
   );
 };
 
